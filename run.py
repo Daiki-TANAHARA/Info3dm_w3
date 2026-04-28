d0ed457b8551ce2a1643a00c5f7abab910335b14
Daiki TANAHARA <e245725@ie.u-ryukyu.ac.jp>
2026-04-28 09:51:06 +0900

    ver2

--- run.py
+++ run.py
@@ -3,15 +3,27 @@
 import datasets
 
 X,Y = datasets.load_linear_example1()
+"""
 print(X)
 print(X[0])
 print(Y)
+"""
 
 #ver1
 
 import regression
 
 model = regression.LinearRegression()
+"""
 print(model.x)
+"""
 
+#ver2
+
+import importlib
+
+importlib.reload(regression)
+model = regression.LinearRegression()
+print(model.fit(X, Y))
+print(model.theta)
 
